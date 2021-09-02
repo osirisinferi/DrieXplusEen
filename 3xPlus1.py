b778abc799e0f75af8347ef172259a00e1581349
OsirisInferi <github@flut.demon.nl>
2021-09-02 18:43:09 +0200

    Don't use `round` as variable, as it's an built-in function too

--- 3xPlus1.py
+++ 3xPlus1.py
@@ -1,20 +1,20 @@
 from sys import exit
 
 
-def collatz(number, round):
+def collatz(number, i):
     if number == 1:
         return 1
     elif (number % 2) > 0:
         result = 3 * number + 1
     else:
         result = int(number / 2)
-    print("Ronde: {}, oude getal: {}, nieuwe getal: {}".format(round, number, result))
-    return result, round+1
+    print("Ronde: {}, oude getal: {}, nieuwe getal: {}".format(i, number, result))
+    return result, i+1
 
 
 def main():
     getal = None
-    start = 1
+    i = 1
 
     try:
         getal = int(input("Vul begin-getal in: "))
@@ -23,7 +23,7 @@ def main():
         exit(1)
 
     while getal != 1:
-        getal, start = collatz(getal, start)
+        getal, i = collatz(getal, i)
 
 
 if __name__ == "__main__":
