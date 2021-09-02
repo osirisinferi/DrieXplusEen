8c81151732d3c24e37b04e80e11eea3ef061f46d
OsirisInferi <github@flut.demon.nl>
2021-09-02 18:13:20 +0200

    Initiele poging GJ

--- 3xPlus1.py
+++ 3xPlus1.py
@@ -1,1 +1,28 @@
-print("Hoi GJ")
+from sys import exit
+
+
+def collatz(number, round):
+    if number == 1:
+        return 1
+    elif (number % 2) > 0:
+        result = 3 * number + 1
+    else:
+        result = int(number / 2)
+    print("Ronde: {}, oude getal: {}, nieuwe getal: {}".format(round, number, result))
+    collatz(result, round+1)
+
+
+def main():
+    start = None
+
+    try:
+        start = int(input("Enter starting point: "))
+    except ValueError:
+        print("Oeps, graag een positief heel getal boven 0 invullen.")
+        exit(1)
+
+    collatz(start, 1)
+
+
+if __name__ == "__main__":
+    main()
